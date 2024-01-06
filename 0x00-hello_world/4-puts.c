d9d475e960e7f5ffea8a3e0dd753157091d73d73
Hossam <Hossam@gmail.com>
2024-01-06 11:32:10 -0800

    That's the good program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point 
+ *
+ *Description: function that prints message with puts function
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzlen");
+	return (0);
+}
